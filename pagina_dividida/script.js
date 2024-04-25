420ed12fade25747e099efdcf4da75e8475ff6f7
Carlos Octavio Otero <oterocarlosprogramacion@gmail.com>
2024-04-24 23:33:31 -0300

    Creacion Pagina Dividida
    Creación del proyecto página dividida

---
+++ pagina_dividida/script.js
@@ -0,0 +1,9 @@
+const izquierda = document.querySelector('.izquierda')
+const derecha = document.querySelector('.derecha')
+const container = document.querySelector('.container')
+
+izquierda.addEventListener('mouseenter', () => container.classList.add('hover-izquierda'))
+izquierda.addEventListener('mouseleave', () => container.classList.remove('hover-izquierda'))
+
+derecha.addEventListener('mouseenter', () => container.classList.add('hover-derecha'))
+derecha.addEventListener('mouseleave', () => container.classList.remove('hover-derecha'))
